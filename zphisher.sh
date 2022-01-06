8b3cdf6489ab13793a88d5dcb1d9f64bb33b1fea
Hshsk <97208074+Hshsk@users.noreply.github.com>
2022-01-06 09:06:47 +0530

    Update zphisher.sh

--- zphisher.sh
+++ zphisher.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ##   Zphisher 	: 	Automated Phishing Tool
-##   Author 	: 	TAHMID RAYAT 
+##   Author 	: 	devil king
 ##   Version 	: 	2.2
 ##   Github 	: 	https://github.com/htr-tech
 
